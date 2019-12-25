060a2698b556b4b59b5d611dc4bd954a25ecc235
baknayeon <30717701+baknayeon@users.noreply.github.com>
2019-12-26 02:09:15 +0900

    add css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,53 @@
 import React from "react"
+import { Link, withRouter } from "react-router-dom"
+import styled from 'styled-components'
 
-export default () => (
-    <header>
-        <ul>
-            <li>
-                <a href="/">Movices</a>
-            </li>
-            <li>
-                <a href="/tv">TV</a>
-            </li>
-            <li>
-                <a href="/search">Search</a>
-            </li>
-        </ul>
-    </header>
+const Header = styled.header`
+    color:white;
+    position:fixed;
+    top:10px;
+    left:10px;
+    width:100%;
+    height:50px;
+    display:flex;
+    align-items:center;
+    padding:10px;
+    background-color:rgba(20, 20, 20, 0.5)
+    z-index: 10;
+    box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`
+const List = styled.ul `
+    display:flex;
+`
+
+const Item = styled.li `
+    height:50px;
+    width: 50px;
+    text-align:center;
+    border-bottom: 3px solid
+    ${props => (props.current ? "#3498db" : "transparent")};
+    transition:border-bottom 0.5s ease-in-out;
+`
+const SLink = styled(Link)`
+    height:50px
+    display: flex;
+    align-items:center;
+    justify-content:center;
+`;
+
+export default withRouter( ({location: {pathname}}) =>(
+        <Header>
+            <List>
+                <Item current={pathname === "/"? true:false}>
+                    <SLink to="/">Movices</SLink>
+                </Item>
+                <Item current={pathname ==="/tv"? true:false}>
+                    <SLink to="/tv">TV</SLink>
+                </Item>
+                <Item current={pathname === "/search"? true:false}>
+                    <SLink to="/search">Search</SLink>
+                </Item>
+            </List>
+        </Header>
+    )   
 )
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,10 +3,12 @@ import {BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom
 import Home from "Routes/Home"
 import Search from "Routes/Search"
 import TV from "Routes/TV"
+import Header from "Components/Header"
 
 export default () => {
     return (
     <Router>
+        <Header/>
         <Switch>
             <Route path="/" exact component={Home}/>
             <Route path="/tv" component={TV}/>
